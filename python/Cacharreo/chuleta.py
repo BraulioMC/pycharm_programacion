f33f99aa6dce28a7901dc7dc1a8e54503665a8c1
A18BraulioMC <braulio_cerdido@hotmail.es>
2018-10-30 19:24:43 +0100

    Guardando ideas

---
+++ python/Cacharreo/chuleta.py
@@ -0,0 +1,15 @@
+s = "HOla que tal"
+print(s)
+
+print(s.upper())
+print(s.title())
+print(s.capitalize())
+
+print("==============")
+print("En la frase \"Donde esta Wally\" lo encontramos en la posicion:", "Dónde está Wally?".find("Wally"))
+
+
+print("==============")
+print("Formas de reemplazar texto")
+print(s.replace("que", "lala", 1)) # Reemplaza "que" por "lala" una vez
+
