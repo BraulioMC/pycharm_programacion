174aaac7e0bd59143e2b48ad440fa5265807e73e
A18BraulioMC <braulio_cerdido@hotmail.es>
2018-11-09 20:30:03 +0100

    Ejercicio5 - Sin empezar

---
+++ python/Tarea2/Ejercicio5.py
@@ -0,0 +1,12 @@
+'''
+    Haz un programa que calcule la menor de cinco palabras dadas, es decir, la
+    primera de las cinco en orden alfabético. El programa no distinguirá entre
+    mayúsculas y minúsculas
+'''
+
+palabra_1 = input("Introduce la primera palabra:\n")
+palabra_2 = input("Introduce la segunda palabra:\n")
+palabra_3 = input("Introduce la tercera palabra:\n")
+palabra_4 = input("Introduce la cuarta palabra:\n")
+palabra_5 = input("Introduce la quinta palabra:\n")
+
