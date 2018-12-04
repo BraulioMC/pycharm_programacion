80cad6ba7d758921995290aa9fab25104f682b0b
braulio.mosquera <braulio.mosqueracupeiro@plexus.es>
2018-12-04 19:11:32 +0100

    Ejercicio2 funciones - Dibujando figuras (funcional)

--- python/Tarea2/Funciones/Ejercicio2.py
+++ python/Tarea2/Funciones/Ejercicio2.py
@@ -5,3 +5,28 @@
 	en que mostrará un mensaje de despedida y finalizará
 '''
 
+
+try:
+    print("========== EL FIGURILLAS ==========")
+    print("Vamos a dibujar figuras ( CTRL-C para terminar )")
+    selecion = int(input("Escoge el tipo (1 - Cuadrado, 2 - Triángulo): "))
+    altura = int(input("Cuál es la altura: "))
+    relleno = input("Y el relleno: ")
+except ValueError:
+    print("Debe ser un número")
+    exit()
+
+try:
+    if selecion == 1:
+        for i in range(altura):
+            print((relleno + " ") * altura)
+    elif selecion == 2:
+        cont = 1
+        formato = ("{:^" + str(altura * altura//2) + "}")
+        for i in range(altura):
+            print(formato.format((relleno + " ") * cont))
+            cont += 1
+    else:
+        print("Debes seleccionar entre valor 1 y 2")
+except NameError:
+    pass
