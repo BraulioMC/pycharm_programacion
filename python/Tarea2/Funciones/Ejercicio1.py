74dbb635a129f7f371916468c92ca83928c8f884
braulio.mosquera <braulio.mosqueracupeiro@plexus.es>
2018-12-04 18:14:36 +0100

    Ejercicio1 de funciones

---
+++ python/Tarea2/Funciones/Ejercicio1.py
@@ -0,0 +1,10 @@
+'''
+	A partir del código del programa del cálculo de la letra de DNI, crea una función llamada calcula_letra_DNI que
+	reciba como parámetro el númro de DNI y devuelva:
+		●	La letra del DNI si todo fue correcto
+		●	El valor "ERR" si no se pudo calcular debido a que el argumento era incorrecto
+	
+	Crea un programa que llame a la función con valores correctos e incorrecto de número de DNI para probarla en
+	diferentes situaciones
+'''
+
