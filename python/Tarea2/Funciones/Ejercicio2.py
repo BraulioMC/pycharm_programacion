41e52dda5bc3d36a7795a291a116d6d445038813
braulio.mosquera <braulio.mosqueracupeiro@plexus.es>
2018-12-04 18:36:22 +0100

    Ejercicio2 funciones - Dibujando figuras

---
+++ python/Tarea2/Funciones/Ejercicio2.py
@@ -0,0 +1,7 @@
+'''
+	Crea un programa que dibuje figuras. Se podrá seleccionar entre dos tipos de figuras, el cuadrado y el triángulo.
+	Cada tipo de figura será dibujada por una función diferente. En ambos casos se pedirá la altura de la figura y
+	el carácter utilizado para rellenarla. El programa se seguirá ejecutando hasta que se presione CTRL-C,
+	en que mostrará un mensaje de despedida y finalizará
+'''
+
