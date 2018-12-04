0414ba25b0cdb9e5adea406fc435031a569ef357
braulio.mosquera <braulio.mosqueracupeiro@plexus.es>
2018-12-04 18:24:10 +0100

    Ejercicio1 de funciones - Primer punto ok

--- python/Tarea2/Funciones/Ejercicio1.py
+++ python/Tarea2/Funciones/Ejercicio1.py
@@ -6,5 +6,27 @@
 	
 	Crea un programa que llame a la función con valores correctos e incorrecto de número de DNI para probarla en
 	diferentes situaciones
+
++------------------------------------------------------------------------------------------------------------------+
+| RESTO : | 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10 | 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18 | 19 | 20 | 21 | 22 |
++------------------------------------------------------------------------------------------------------------------+
+| LETRA : | t | r | w | a | g | m | y | f | p | d |  x |  b |  n |  j |  z |  s |  q |  v |  h |  l |  c |  k |  e |
++------------------------------------------------------------------------------------------------------------------+
 '''
 
+def calcula_letra_DNI(numero):
+    resto = int((numero % 23))
+    letra = "trwagmyfpdxbnjzsqvhlcke"
+    dni = letra[resto]
+    return dni
+
+# Número de DNI
+numero = int(input("Introduce número de DNI de 8 dígitos: "))
+
+
+if len(str(numero)) != 8:
+    print("Tiene que ser de 8 dígitos")
+else:
+    print("Calculando...")
+    dni = calcula_letra_DNI(numero)
+    print(str(numero )+ str(dni))
