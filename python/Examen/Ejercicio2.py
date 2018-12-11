f9f444cf342316b77ed5af4a7d02539b59fe0883
A18BraulioMC <a18brauliomc@iessanclemente.net>
2018-12-11 22:18:26 +0100

    Examen - Ejercicio 2

--- python/Examen/Ejercicio2.py
+++ python/Examen/Ejercicio2.py
@@ -1,5 +1,3 @@
-num = "-123.56147876868"
-
 def separar_millar(num):
     largo_total = len(num)
     punto = largo_total - 3
@@ -25,29 +23,32 @@ def num_coma(num):
 
 
 
+def procesar_num(num):
 
+    if num.find("-") != -1:
+        num = num[1:]
+        if num.find(".") != -1:
+            resto, decimal = num_punto(num)
+            print("-" + salida_comun(num, decimal, resto))
 
-if num.find("-") != -1:
-    num = num[1:]
-    if num.find(".") != -1:
-        resto, decimal = num_punto(num)
-        print("-" + salida_comun(num, decimal, resto))
+        elif num.find(",") != -1:
+            resto, decimal = num_coma(num)
+            print("-" + salida_comun(num, decimal, resto))
 
-    elif num.find(",") != -1:
-        resto, decimal = num_coma(num)
-        print("-" + salida_comun(num, decimal, resto))
-
-    else:
-        print("1" + separar_millar(num))
-
-else:
-    if num.find(".") != -1:
-        resto, decimal = num_punto(num)
-        print(salida_comun(num, decimal, resto))
-
-    elif num.find(",") != -1:
-        resto, decimal = num_coma(num)
-        print(salida_comun(num, decimal, resto))
+        else:
+            print("-" + separar_millar(num))
 
     else:
-        print(separar_millar(num))
+        if num.find(".") != -1:
+            resto, decimal = num_punto(num)
+            print(salida_comun(num, decimal, resto))
+
+        elif num.find(",") != -1:
+            resto, decimal = num_coma(num)
+            print(salida_comun(num, decimal, resto))
+
+        else:
+            print(separar_millar(num))
+    return ""
+
+print(procesar_num(input("Introduce numero: ")))
