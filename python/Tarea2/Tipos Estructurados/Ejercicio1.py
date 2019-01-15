ce6b253be232077e588927cd0b70adfb5c0353f6
braulio.mosquera <braulio.mosqueracupeiro@plexus.es>
2019-01-15 20:16:39 +0100

    Ejercicio 1 Tareas

---
+++ python/Tarea2/Tipos Estructurados/Ejercicio1.py
@@ -0,0 +1,15 @@
+'''
+Crea un módulo llamado t02_05_mod . Añade al módulo una función denominada
+remove_dup que recibirá como argumento cualquier secuencia iterable (tupla, lista,
+string,...) y devolverá como resultado una tupla con la secuencia original pero sin
+duplicados.
+Incluye en el d ocstring de la función los siguientes casos de uso:
+
+Parámetro                       Resultado
+[]                              ()
+(1, 2, 3)                       (1, 2, 3)
+(1, 2, 3, 2, 1)                 (1, 2, 3)
+[1, (1, 2), 3, (1, 2), 4, 3]    (1, (1, 2), 3, 4)
+"hooooooolaaaaa"                ('h', 'o', 'l', 'a')
+'''
+
