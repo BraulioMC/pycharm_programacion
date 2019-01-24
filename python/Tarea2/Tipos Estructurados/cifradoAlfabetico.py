2d886c66f3abb126e5def03399dc0eaa43bc532d
braulio.mosquera <braulio.mosqueracupeiro@plexus.es>
2019-01-24 19:20:44 +0100

    Funciones de crear dic, cifrar y descifrar completado

--- python/Tarea2/Tipos Estructurados/cifradoAlfabetico.py
+++ python/Tarea2/Tipos Estructurados/cifradoAlfabetico.py
@@ -29,14 +29,13 @@ función elimina_tildes del módulo mod_u05
 según la clave proporcionada
 '''
 
-def tablero_klave(var_klave):
+def genera_dic(var_klave):
     var_klave_dic = {}
     var_klave_list = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S",
                       "T", "U", "V", "W", "X", "Y", "Z"]
 
     var_klave_list_len = len(var_klave_list)
     var_klave_len = len(var_klave)
-    #var_set_klave = set(var_klave)
 
     for i in range(0, var_klave_len):
         key = var_klave_list[i]
@@ -58,10 +57,40 @@ def tablero_klave(var_klave):
         var_klave_dic.update({key:valor})
         count += 1
 
+    var_klave_dic.update({" ":" "})
     return var_klave_dic
 
+def cifrar_mensaje(frase_cifrar):
+    salida = ""
+    for i in frase_cifrar:
+        if (i == " "):
+            #print(" ", end='')
+            salida = salida + " "
+        else:
+            #print(var_klave_dic[i], end='')
+            salida = salida + var_klave_dic[i]
+    return salida
+
+def descifrar_codigo(mensaje_cifrado):
+    salida = ""
+    for i in mensaje_cifrado:
+        var_pos = list(var_klave_dic.values()).index(i)
+        lista = list(var_klave_dic.keys())
+        salida = salida + lista[var_pos]
+    return salida
+
+# PARTE 1 - GENERAMOS EL DICCIONARIO CON LA KLAVE PARA CIFRAR
 klave = input("Introduce klave: ")
+var_klave_dic = genera_dic(klave)
 
-var_klave_dic = tablero_klave(klave)
+# PARTE 2 - INTRODUCIMOS FRASE A CIFRAR
 
-print(var_klave_dic)
+frase_cifrar = input("Introduce frase para cifrar: ")
+mensaje_cifrado = cifrar_mensaje(frase_cifrar)
+
+print("Mensaje cifrado: ", mensaje_cifrado)
+
+# PARTE 3 - DESCIFRAMOS MENSAJE
+
+mensaje = descifrar_codigo(mensaje_cifrado)
+print("Mensaje descifrado: ", mensaje)
