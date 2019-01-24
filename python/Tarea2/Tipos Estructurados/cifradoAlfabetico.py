aac12f888762ed3ca0e938b2a5ccdfe5ec485bce
braulio.mosquera <braulio.mosqueracupeiro@plexus.es>
2019-01-24 20:06:46 +0100

    Finalizamos pasos de ejecucion en funciones, falta comprobar inicializado de diccionario

--- python/Tarea2/Tipos Estructurados/cifradoAlfabetico.py
+++ python/Tarea2/Tipos Estructurados/cifradoAlfabetico.py
@@ -3,6 +3,7 @@
 Dentro de los cifrados de sustitución monoalfabética, existe una variante del cifrado César
 consistente en la introducción de una clave, sin caracteres repetidos, a partir de la cual se
 creará el alfabeto de cifrado por sustitución.
+
 Para crear el alfabeto de cifrado, hacemos coincidir las letras del alfabeto con la clave,
 creando las parejas de sustitución. Cuando lleguemos al final de la clave, vamos añadiendo
 en orden el resto de los caracteres del alfabeto que no hayan sido usados.
@@ -10,24 +11,32 @@ Por ejemplo, supongamos que la clave es: ZAFIRO . La hacemos coincidir con los p
 caracteres del alfabeto:
 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 Z A F I R O
+
 A continuación, seguimos añadiendo en orden los restantes caracteres no usados:
 A B C D E F G H I J K L M N O P Q R S T U V W X Y Z
 Z A F I R O B C D E G H J K L M N P Q S T U V W X Y
+
 Ahora ya podemos usar esa tabla tanto para cifrar como descifrar:
 “ESTO ES UN MENSAJE SECRETO” <<<<< >>>>> “ RQSL RQ TK JRKQZER QRFPRSL ”
+
 Crea un programa que permita cifrar y descifrar mensajes utilizando este sistema.
 Debes tener en cuenta que:
 ● Puedes implementar la tabla de cifrado de diferentes maneras:
 1. empleando dos listas, una para cada alfabeto
 2. almacenando la tabla de cifrado en un único diccionario
 3. creando dos diccionarios, uno para cifrar y otro para descifrar
+
 ● Debes comprobar que la clave introducida no contenga la letra Ñ y no contenga
 letras repetidas.
+
 ● Para eliminar las tildes, tanto del mensaje como de la clave, puedes hacer uso de la
 función elimina_tildes del módulo mod_u05
+
 ● Antes de cifrar/descifrar debes comprobar que se ha inicializado la tabla de cifrado
 según la clave proporcionada
 '''
+import sys, os
+from t02_05_mod import elimina_tildes
 
 def genera_dic(var_klave):
     var_klave_dic = {}
@@ -79,18 +88,35 @@ def descifrar_codigo(mensaje_cifrado):
         salida = salida + lista[var_pos]
     return salida
 
+def procesa_klave(klave):
+    for i in klave:
+        if (i == "ñ" or i == "Ñ"):
+            print("No puede contener 'ñ' o 'Ñ'")
+            sys.exit()
+
+    klave = elimina_tildes(klave)
+    klave = klave.upper()
+    var_klave_dic = genera_dic(klave)
+    return var_klave_dic
+
+def procesa_frase(frase_cifrar):
+    frase_cifrar = elimina_tildes(frase_cifrar)
+    frase_cifrar = frase_cifrar.upper()
+    mensaje_cifrado = cifrar_mensaje(frase_cifrar)
+    return mensaje_cifrado
+
 # PARTE 1 - GENERAMOS EL DICCIONARIO CON LA KLAVE PARA CIFRAR
 klave = input("Introduce klave: ")
-var_klave_dic = genera_dic(klave)
+var_klave_dic = procesa_klave(klave)
 
 # PARTE 2 - INTRODUCIMOS FRASE A CIFRAR
-
 frase_cifrar = input("Introduce frase para cifrar: ")
-mensaje_cifrado = cifrar_mensaje(frase_cifrar)
-
-print("Mensaje cifrado: ", mensaje_cifrado)
+mensaje_cifrado = procesa_frase(frase_cifrar)
 
 # PARTE 3 - DESCIFRAMOS MENSAJE
-
 mensaje = descifrar_codigo(mensaje_cifrado)
-print("Mensaje descifrado: ", mensaje)
+
+# PARTE 4 - MOSTRAMOS MENSAJE CIFRADO Y DESCIFRADO
+os.system('cls||clear')
+print("Mensaje cifrado:     ", mensaje_cifrado)
+print("Mensaje descifrado:  ", mensaje)
