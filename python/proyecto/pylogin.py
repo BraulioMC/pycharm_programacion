912bd0d37d30a3c5c66f1ad9d72608eb46ab6cc2
braulio.mosquera <braulio.mosqueracupeiro@plexus.es>
2019-05-23 13:40:39 +0200

    añade constantes

--- python/proyecto/pylogin.py
+++ python/proyecto/pylogin.py
@@ -4,15 +4,37 @@ from tkinter import Tk, Label, Entry, GROOVE, Button
 from tkinter import messagebox
 from db import loginDB, registerUser, closeDB
 
-bgclr="#282828"
-fgclr="#cecece"
-clr='#004a95'
+__BGCLR="#282828"
+__FGCLR="#cecece"
+__CLR='#004a95'
+__FGCOLOR='white'
+__GEOMETRY="420x400"
+__TITLE="Login GUI"
+__FONT_PASS=("blod",15)
+__WIDTH=40
+__FONT=10
+__LABEL_X=20
+__LABEL_Y=40
+__USER_X=20
+__USER_Y=80
+__PASS_X=20
+__PASS_Y=160
+__BD=5
+__WARN_LABEL_X=80
+__WARN_LABEL_Y=200
+__BUTTOM_LOGIN_X=20
+__BUTTOM_LOGIN_Y=240
+__BUTTOM_REG_X=20
+__BUTTOM_REG_Y=300
+__HIGHLIGHT_LETTER=2
+__HIGHLIGHT_BOTTOM=4
+
 
 def login():
 
     seed = password_Entry.get()
     h = hashlib.sha256(seed.encode())
-
+    
     access = loginDB(user_Entry.get(), h.hexdigest())
 
     if access:
@@ -34,77 +56,78 @@ def register():
         messagebox.showinfo('Registro', 'El usuario ya existe')
 
 w=Tk()
-w.title("Login GUI")
-w.geometry("420x400")
-w.config(bg=bgclr)
+w.title(__TITLE)
+w.geometry(__GEOMETRY)
+w.config(bg=__BGCLR)
 w.resizable()
 
 user=Label(w,
             text="User",
-            font=("blod",15),
-            bg=bgclr,
-            fg=fgclr)
-user.place(x=20,y=40)
+            font=(__FONT_PASS),
+            bg=__BGCLR,
+            fg=__FGCLR)
+user.place(x=__LABEL_X,y=__LABEL_Y)
 
-user_Entry=Entry(w,bg=bgclr,
-            fg="white",
+user_Entry=Entry(w,bg=__BGCLR,
+            fg=__FGCOLOR,
             relief=GROOVE,
-            highlightcolor="white",
-            highlightthickness=2,
-            highlightbackground=clr,
-            width=40,
-            font=10,
-            bd=5)
-user_Entry.place(x=20,y=80)
+            highlightcolor=__FGCOLOR,
+            highlightthickness=__HIGHLIGHT_LETTER,
+            highlightbackground=__CLR,
+            width=__WIDTH,
+            font=__FONT,
+            bd=__BD)
+user_Entry.place(x=__USER_X,y=__USER_Y)
 
 password=Label(w,
             text="Password",
-            font=("blod",15),
-            bg=bgclr,
-            fg=fgclr)
-password.place(x=20,y=120)
+            font=(__FONT_PASS),
+            bg=__BGCLR,
+            fg=__FGCLR)
+password.place(x=__PASS_X,y=__PASS_Y)
 
-password_Entry=Entry(w,bg=bgclr,
-            fg="white",
+password_Entry=Entry(w,bg=__BGCLR,
+            fg=__FGCOLOR,
             relief=GROOVE,
-            highlightcolor="white",
-            highlightthickness=2,
-            highlightbackground=clr, 
-            width=40,
-            font=10,
+            highlightcolor=__FGCOLOR,
+            highlightthickness=__HIGHLIGHT_LETTER,
+            highlightbackground=__CLR, 
+            width=__WIDTH,
+            font=__FONT,
             show="*",
-            bd=5)
-password_Entry.place(x=20,y=160)
+            bd=__BD)
+password_Entry.place(x=__PASS_X,y=__PASS_Y)
 
 warn=Label(w,
             font=("blod",10),
-            bg=bgclr)
+            bg=__BGCLR)
            
-warn.place(x=80,y=200)
+warn.place(x=__WARN_LABEL_X,y=__WARN_LABEL_Y)
 
 button=Button(w,
             text="Login",
-            bg=clr,
-            fg="white",
+            bg=__CLR,
+            fg=__FGCOLOR,
             relief=GROOVE,
-            highlightcolor=clr,
-            highlightthickness=4,
-            width=40,
-            font=10,
+            highlightcolor=__CLR,
+            highlightthickness=__HIGHLIGHT_BOTTOM,
+            width=__WIDTH,
+            font=__FONT,
             command=login)
-button.place(x=20,y=240)
+button.place(x=__BUTTOM_LOGIN_X,y=__BUTTOM_LOGIN_Y)
 
 button=Button(w,
             text="Register",
-            bg=clr,
-            fg="white",
+            bg=__CLR,
+            fg=__FGCOLOR,
             relief=GROOVE,
-            highlightcolor=clr,
-            highlightthickness=4,
-            width=40,
-            font=10,
+            highlightcolor=__CLR,
+            highlightthickness=__HIGHLIGHT_BOTTOM,
+            width=__WIDTH,
+            font=__FONT,
             command=register)
-button.place(x=20,y=300)
+button.place(x=__BUTTOM_REG_X,y=__BUTTOM_REG_Y)
+
 
 if __name__ == "__main__":
     try:
